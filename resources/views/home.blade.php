ac67d9ea3bee4a88f389eaa331e13ac98ec391f8
linares82 <linares82@gmail.com>
2017-07-17 19:55:46 -0500

    ejemplo tablero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,64 @@
 
     <div class="row ">
         <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Tablero</div>
-
-                <div class="panel-body">
-                    Estas identificado!
+            <!-- Main content -->
+            <section class="content">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                    <div class="inner">
+                    <h3>47</h3>
+                    <p>Pendientes del mes</p>
+                    </div>
+                    <div class="icon">
+                    <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Más Información <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
-            </div>
+                </div><!-- ./col -->
+                <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                    <div class="inner">
+                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+                    <p>Concretados del Mes</p>
+                    </div>
+                    <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Más información <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+                </div><!-- ./col -->
+                <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                    <div class="inner">
+                    <h3>44</h3>
+                    <p>Asignados en el mes</p>
+                    </div>
+                    <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Más información <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+                </div><!-- ./col -->
+                <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-red">
+                    <div class="inner">
+                    <h3>65</h3>
+                    <p>Rechazados</p>
+                    </div>
+                    <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Mas Información <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+                </div><!-- ./col -->
+            </div><!-- /.row -->
+            <!-- Main row -->
         </div>
     </div>
 
