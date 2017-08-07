1dae71527b152f50c5eb5ecec051664109c458e4
linares82 <linares82@gmail.com>
2017-08-07 13:27:04 -0500

    relaciones con planteles

--- app/Traits/RelationManagerTrait.php
+++ app/Traits/RelationManagerTrait.php
@@ -11,6 +11,7 @@ namespace App\Traits;
 use App\Empleado;
 use App\Nivel;
 use Auth;
+use DB;
 trait RelationManagerTrait {
 
     /**
@@ -59,35 +60,59 @@ trait RelationManagerTrait {
           }
           
           if($relationAppName=="Nivel" and Auth::user()->can('IfiltroNivelXplantel')){
-            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)->pluck($relationAppArray['relation_display_column'], 'id');
+            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)
+                                                      ->join('plantels as p', 'p.id', '=', 'nivels.plantel_id')
+                                                      ->select(DB::raw('concat(name, "-",razon) as relacion'))
+                                                      ->pluck('relacion', 'id');
             //dd($relatedObjList);
           }
           if($relationAppName=="Grado" and Auth::user()->can('IfiltroGradoXplantel')){
-            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)->pluck($relationAppArray['relation_display_column'], 'id');
+            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)
+                                                      ->join('plantels as p', 'p.id', '=', 'grados.plantel_id')
+                                                      ->select(DB::raw('concat(name, "-",razon) as relacion'))
+                                                      ->pluck('relacion', 'id');
             //dd($relatedObjList);
           }
           if($relationAppName=="Curso" and Auth::user()->can('IfiltroCursoXplantel')){
-            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)->pluck($relationAppArray['relation_display_column'], 'id');
+            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)
+                                                      ->join('plantels as p', 'p.id', '=', 'cursos.plantel_id')
+                                                      ->select(DB::raw('concat(name, "-",razon) as relacion'))
+                                                      ->pluck('relacion', 'id');
             //dd($relatedObjList);
           }
           if($relationAppName=="Subcurso" and Auth::user()->can('IfiltroSubcursoXplantel')){
-            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)->pluck($relationAppArray['relation_display_column'], 'id');
+            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)
+                                                      ->join('plantels as p', 'p.id', '=', 'subcursos.plantel_id')
+                                                      ->select(DB::raw('concat(name, "-",razon) as relacion'))
+                                                      ->pluck('relacion', 'id');
             //dd($relatedObjList);
           }
           if($relationAppName=="Diplomado" and Auth::user()->can('IfiltroDiplomadoXplantel')){
-            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)->pluck($relationAppArray['relation_display_column'], 'id');
+            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)
+                                                      ->join('plantels as p', 'p.id', '=', 'diplomados.plantel_id')
+                                                      ->select(DB::raw('concat(name, "-",razon) as relacion'))
+                                                      ->pluck('relacion', 'id');
             //dd($relatedObjList);
           }
           if($relationAppName=="Subdiplomado" and Auth::user()->can('IfiltroSubdiplomadoXplantel')){
-            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)->pluck($relationAppArray['relation_display_column'], 'id');
+            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)
+                                                      ->join('plantels as p', 'p.id', '=', 'subdiplomados.plantel_id')
+                                                      ->select(DB::raw('concat(name, "-",razon) as relacion'))
+                                                      ->pluck('relacion', 'id');
             //dd($relatedObjList);
           }
           if($relationAppName=="Otro" and Auth::user()->can('IfiltroOtroXplantel')){
-            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)->pluck($relationAppArray['relation_display_column'], 'id');
+            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)
+                                                      ->join('plantels as p', 'p.id', '=', 'otros.plantel_id')
+                                                      ->select(DB::raw('concat(name, "-",razon) as relacion'))
+                                                      ->pluck('relacion', 'id');
             //dd($relatedObjList);
           }
           if($relationAppName=="Subotro" and Auth::user()->can('IfiltroSubotroXplantel')){
-            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)->pluck($relationAppArray['relation_display_column'], 'id');
+            $relatedObjList = $relationAppArray['app']::where('plantel_id', '=', $e->plantel_id)
+                                                      ->join('plantels as p', 'p.id', '=', 'subotros.plantel_id')
+                                                      ->select(DB::raw('concat(name, "-",razon) as relacion'))
+                                                      ->pluck('relacion', 'id');
             //dd($relatedObjList);
           }
           //$relatedObjList = $relationAppArray['app']::pluck($relationAppArray['relation_display_column'], 'id');
