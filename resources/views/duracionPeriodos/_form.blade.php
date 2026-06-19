b4d3bbbe510d3025a5bc473b9af606d9277e57bc
linares82 <linares82@gmail.com>
2026-06-18 18:23:15 -0600

    cambio de input excepcion por materias no aprobadas

--- resources/views/duracionPeriodos/_form.blade.php
+++ resources/views/duracionPeriodos/_form.blade.php
@@ -5,20 +5,6 @@
       <span class="help-block">{{ $errors->first("name") }}</span>
       @endif
    </div>
-   <div class="form-group col-md-4 @if($errors->has('bloqueo_cantidad_reprobadas')) has-error @endif">
-      <label for="bloqueo_cantidad_reprobadas-field">Bloqueo por Materias Reprobadas</label>
-      {!! Form::text("bloqueo_cantidad_reprobadas", null, array("class" => "form-control", "id" => "bloqueo_cantidad_reprobadas-field")) !!}
-      @if($errors->has("bloqueo_cantidad_reprobadas"))
-      <span class="help-block">{{ $errors->first("bloqueo_cantidad_reprobadas") }}</span>
-      @endif
-   </div>
-   <div class="form-group col-md-4 @if($errors->has('clis_excepcion_reprobadas')) has-error @endif">
-      <label for="clis_excepcion_reprobadas-field">Excepción Materias Reprobadas(ids de clientes separados por coma y sin espacios)</label>
-      {!! Form::text("clis_excepcion_reprobadas", null, array("class" => "form-control", "id" => "clis_excepcion_reprobadas-field")) !!}
-      @if($errors->has("clis_excepcion_reprobadas"))
-      <span class="help-block">{{ $errors->first("clis_excepcion_reprobadas") }}</span>
-      @endif
-   </div>
    <div class="form-group col-md-4 @if($errors->has('bloqueo_cantidad_extras')) has-error @endif">
       <label for="bloqueo_cantidad_extras-field">Bloqueo por Materias Extras</label>
       {!! Form::text("bloqueo_cantidad_extras", null, array("class" => "form-control", "id" => "bloqueo_cantidad_extras-field")) !!}
@@ -33,3 +19,17 @@
       <span class="help-block">{{ $errors->first("id_tipo_periodo_certificado") }}</span>
       @endif
    </div>                
+   <div class="form-group col-md-4 @if($errors->has('bloqueo_cantidad_reprobadas')) has-error @endif">
+      <label for="bloqueo_cantidad_reprobadas-field">Bloqueo por Materias Reprobadas</label>
+      {!! Form::text("bloqueo_cantidad_reprobadas", null, array("class" => "form-control", "id" => "bloqueo_cantidad_reprobadas-field")) !!}
+      @if($errors->has("bloqueo_cantidad_reprobadas"))
+      <span class="help-block">{{ $errors->first("bloqueo_cantidad_reprobadas") }}</span>
+      @endif
+   </div>
+   <div class="form-group col-md-8 @if($errors->has('clis_excepcion_reprobadas')) has-error @endif">
+      <label for="clis_excepcion_reprobadas-field">Excepción Materias Reprobadas(ids de clientes separados por coma y sin espacios)</label>
+      {!! Form::textArea("clis_excepcion_reprobadas", null, array("class" => "form-control", "id" => "clis_excepcion_reprobadas-field", 'rows'=>3)) !!}
+      @if($errors->has("clis_excepcion_reprobadas"))
+      <span class="help-block">{{ $errors->first("clis_excepcion_reprobadas") }}</span>
+      @endif
+   </div>
